3ec76e5414ea6c4ed5b0c574d634690e331a2201
daniellee0 <donloby@uw.edu>
2018-11-14 23:06:09 -0800

    Add lab files

--- day-11/csv-demo/js/index.js
+++ day-11/csv-demo/js/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
 // Use d3 to load the csv file using a *relative path*
+// Don't use .. because you need to reference url based on where the index.html is
+d3.csv("data/websites.csv").then(function(data) {
+    console.log(data);
+
     // Iterate through the data and append a new <li> for each item
     // The <li> should contain a link (<a>) to the website and 
     // a <span> with the description about the website
-    
+    data.map(function(d) {
+        let li = $('<li>');
+        let a = $('<a href=" "> </a>');
+        li.append(a);
+        li.append(`<span>: ${d.description}</span>`);
+        $('ul').append(li);
+    })
+});
--- day-12/js/index.js
+++ day-12/js/index.js
@@ -4,12 +4,26 @@
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
-
+    constructor(img="https://www.petmd.com/sites/default/files/petmd-shaking-puppy.jpg", 
+    sound="bark") {
+        this.img = img;
+        this.sound = sound;
+    }
     // Method for "speaking" using responsiveVoice
-
+    bark() {
+        responsiveVoice.speak(this.sound);
+    }
     // Render a Div that you can click on to bark
-
+    render() {
+        let puppyCard = $('<div>');
+        puppyCard.attr('title', `puppy that says ${this.sound}`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.attr('background-image', `url(${this.img})`);
+        puppyCard.append(`<img src=${this.img} class='puppyCard' />`);  // Fix this...
+        puppyCard.click( () => this.bark() );
+    }
 }
+
 
 
 // Class Form
@@ -52,23 +66,47 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
-
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        console.log(this);
+        this.puppyList.push({
+            url: img,
+            sound: sound
+        });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
+        let form = new PuppyForm(this.addPuppy.bind(this));
+        this.parentElement.append(form.render());
 
         // Append puppy list element to parent (in a wrapper div)
-
+        let puppyWrapper = $('<div class= "row">');
+        this.parentElement.append(puppyWrapper);
+        this.puppyList.map( (puppyInfo) => {
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        });
     }
 }
 
 // Create a new app with a single puppy
-
+let app = new PuppyApp(
+    $('#content'),
+    [{
+        url: 'https://www.petmd.com/sites/default/files/petmd-shaking-puppy.jpg',
+        bark: 'bark'
+    }]
+);
 
 // Render the app
+app.render();
